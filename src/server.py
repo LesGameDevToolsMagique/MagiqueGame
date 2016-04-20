29ac83c7c2c85b4e6bf13a07dc777fc925ebb879
DomoDaisuki <domodaisuki@gmail.com>
2016-04-20 18:22:23 +0100

    server inclusion and expressions json file

--- src/server.py
+++ src/server.py
@@ -1,14 +1,74 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
-#
-#   Script start
-#
+import socketserver, subprocess, sys
+from threading import Thread
+from pprint import pprint
+from common.connection import Connection
+import json
+import re
+import random
 
-def main():
-    print("%s" % 'server')
+my_unix_command = ['bc']
+HOST = 'localhost'
+PORT = 12321
 
+with open('expressions.json') as data_file:
+    JSONexp = json.load(data_file)['expressions']
+                
+class JSONHandler:
+    def ranexp(self):
+        return (random.choice(JSONexp))
 
+class gameLogic:
+    """
+    """
 
+    def __init__(self):
+        self.currentExp = None
 
-if __name__ == '__main__':
-    main()
+    def newExpression(self):
+        self.currentExp = JSONHandler.ranexp(self)
+        self.request.send(str(self.currentExp).encode('utf-8'))
+
+    def inputCheck(self, text, force):
+        if ('aze' in text):
+            self.newExpression()
+            
+    def responseCheck(self, response):
+        print(self.currentExp['answer'])
+        if (response is self.currentExp['answer']):
+            return True
+        return False
+
+class SingleTCPHandler(socketserver.BaseRequestHandler):
+    def handle(self):
+        print ("%s connected", self.client_address[0])
+        go = True
+        while go:
+            gameLogic.newExpression(self)
+            data = self.request.recv(1024)
+            if not data:
+                go = False
+            else:
+                text = data.decode('utf-8')
+                print("Client wrote: ", text)
+                if (gameLogic.responseCheck(self, text[0]) is True):
+                    print ('ok')
+                else:
+                    go = False
+                    print('ko')
+        print ("%s disconnected", self.client_address[0])
+                            
+class SimpleServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
+    daemon_threads = True
+    allow_reuse_address = True
+            
+def __init__(self, server_address, RequestHandlerClass):
+    socketserver.TCPServer.__init__(self, server_address, RequestHandlerClass)
+                
+if __name__ == "__main__":
+    server = SimpleServer((HOST, PORT), SingleTCPHandler)
+    try:
+        server.serve_forever()
+    except KeyboardInterrupt:
+        sys.exit(0)
